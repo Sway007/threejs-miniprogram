ff3c8cfeccdffc56e895969575711dacc7993343
swayyan <swayyan@tencent.com>
2021-01-12 14:59:49 +0800

    feat: 升级three.js版本, 增加ts支持

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+import * as THREE from "three";
+
+export declare function createScopedThreejs(canvas: any): typeof THREE;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import {atob as _atob} from 'abab';
-import _XMLHttpRequest from './XMLHttpRequest'
-import copyProperties from './copyProperties'
-import EventTarget from "./EventTarget"
+import { atob as _atob } from "abab";
+import _XMLHttpRequest from "./XMLHttpRequest";
+import copyProperties from "./copyProperties";
+import EventTarget from "./EventTarget";
 
 export function createScopedThreejs(canvas) {
   // adapt canvas
@@ -9,62 +9,61 @@ export function createScopedThreejs(canvas) {
   // canvas.clientHeight = canvas.height
   // canvas.clientWidth = canvas.width
 
-  Object.defineProperty(canvas, 'style', {
+  Object.defineProperty(canvas, "style", {
     get() {
       return {
-        width: this.width + 'px',
-        height: this.height + 'px'
-      }
-    }
-  })
+        width: this.width + "px",
+        height: this.height + "px",
+      };
+    },
+  });
 
-  Object.defineProperty(canvas, 'clientHeight', {
+  Object.defineProperty(canvas, "clientHeight", {
     get() {
-      return this.height
-    }
-  })
+      return this.height;
+    },
+  });
 
-  Object.defineProperty(canvas, 'clientWidth', {
+  Object.defineProperty(canvas, "clientWidth", {
     get() {
-      return this.width
-    }
-  })
+      return this.width;
+    },
+  });
 
-  copyProperties(canvas.constructor.prototype, EventTarget.prototype)
+  copyProperties(canvas.constructor.prototype, EventTarget.prototype);
 
   // eslint-disable-next-line
   const document = {
     createElementNS(_, type) {
-      if (type === 'canvas') return canvas
-      if (type === 'img') return canvas.createImage()
-    }
-  }
-  copyProperties(document.constructor.prototype, EventTarget.prototype)
+      if (type === "canvas") return canvas;
+      if (type === "img") return canvas.createImage();
+    },
+  };
+  copyProperties(document.constructor.prototype, EventTarget.prototype);
 
   // eslint-disable-next-line
   const window = {
-    AudioContext: function() {},
+    AudioContext: function () {},
     URL: {},
-  }
-  copyProperties(window.constructor.prototype, EventTarget.prototype)
+  };
+  copyProperties(window.constructor.prototype, EventTarget.prototype);
 
   // eslint-disable-next-line
   const atob = (a) => {
-    return _atob(a)
-  }
+    return _atob(a);
+  };
 
   // eslint-disable-next-line
-  const XMLHttpRequest = _XMLHttpRequest
+  const XMLHttpRequest = _XMLHttpRequest;
 
   const exports = {};
 
   // eslint-disable-next-line
-  const HTMLCanvasElement = undefined
+  const HTMLCanvasElement = undefined;
 
   // three.js source code will be injected here
   // eslint-disable-next-line
-  __INJECT_THREE__
+  __INJECT_THREE__;
 
-  return exports
+  return exports;
 }
-
